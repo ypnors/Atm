5f0cfd00bfbde90da68596088c9899798d63295e
ypnors <godhype@gmail.com>
2017-12-05 11:46:26 +0800

    12/5

--- app/src/main/java/com/ypnors/atm/LoginActivity.java
+++ app/src/main/java/com/ypnors/atm/LoginActivity.java
@@ -1,9 +1,38 @@
 package com.ypnors.atm;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public void login(View view){
+        EditText edUserid=(EditText)findViewById(R.id.userid);
+        EditText edPasswd=(EditText)findViewById(R.id.passwd);
+        String uid =edUserid.getText().toString();
+        String pw=edPasswd.getText().toString();
+        if (uid.equals("jack")&&pw.equals("1234")){
+            Toast.makeText(this,"登入成功",Toast.LENGTH_LONG).show();
+            getIntent().putExtra("LOGIN_USERID",uid);
+            getIntent().putExtra("LOGIN_PASSWD",pw);
+            setResult(RESULT_OK,getIntent());
+            finish();
+        }else{
+            new AlertDialog.Builder(this)
+                    .setTitle("ATM")
+                    .setMessage("登入失敗")
+                    .setPositiveButton("OK",null)
+                    .show();
+        }
+
+    }
+
+    public void cancel(View view){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/ypnors/atm/MainActivity.java
+++ app/src/main/java/com/ypnors/atm/MainActivity.java
@@ -6,13 +6,28 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     boolean logon =false;
-    public static final int FUNC_LOGIN=1;
+   private final static int REQUEST_LOGIN=102;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_LOGIN){
+            if(resultCode == RESULT_OK){
+                String uid=data.getStringExtra("LOGIN_USERID");
+                String pw=data.getStringExtra("LOGIN_PASSWD");
+                Log.d("RESULT",uid+"/"+pw);
+            }else{
+                finish();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if(!logon){
             Intent intent =new Intent(this,LoginActivity.class);
-            startActivityForResult(intent,FUNC_LOGIN);
+            startActivityForResult(intent,REQUEST_LOGIN);
         }
 
 
